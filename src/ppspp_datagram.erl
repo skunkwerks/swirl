8511019c6aefc2058ac7a3594d66b12bd6473b34
Dave Cottlehuber <dch@skunkwerks.at>
2013-07-30 02:17:02 +0200

    datagram: unpack binary into orddict

---
+++ src/ppspp_datagram.erl
@@ -0,0 +1,63 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+%% @author Dave Cottlehuber <dch@jsonified.com>
+%% @doc Library for PPSPP over UDP, aka PPSPP protocol
+%% <p>This module implements a library of functions necessary to
+%% handle the wire-protocol of PPSPP over UDP, including
+%% functions for encoding and decoding datagrams.</p>
+%% @end
+
+-module(ppspp_datagram).
+-include("swerl.hrl").
+
+-ifdef(TEST).
+-include_lib("proper/include/proper.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+-export([handle/1, unpack/2, pack/1]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc unpack a UDP packet into a PPSPP datagram using erlang term format
+%% <p>  Deconstruct PPSPP UDP datagram into multiple erlang terms, including
+%% <ul>
+%% <li>orddict for Transport</li>
+%% <li>list of Messages</li>
+%% <li>orddict for Options</li>
+%% <ul>
+%% A single datagram MAY contain multiple PPSPP messages; these will be handled
+%% recursively as needed.
+%% </p>
+%% @end
+
+%% packet() = [
+%% TODO revisit specs
+%% {transport, ppspp_transport()},
+%% {options, ppspp_options()},
+%% {messages, ppspp_messages()}
+%% ].
+
+%%-spec unpack(ppspp_transport(), packet() -> ppspp_datagram()).
+
+unpack(Transport, <<Channel:?PPSPP_CHANNEL_SIZE, Maybe_Messages/binary>> ) ->
+    ?DEBUG_DGRAM_RECV(Channel),
+    {ok, Parsed_Messages} = ppspp_message:unpack(Maybe_Messages),
+    ?DEBUG_DGRAM_PARSE_OK(Channel),
+    Datagram = orddict:store(messages, Parsed_Messages,
+        orddict:store(channel, Channel, Transport)),
+    {ok, Datagram}.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+handle(_) -> {ok, state}.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+pack(_) -> ok.
