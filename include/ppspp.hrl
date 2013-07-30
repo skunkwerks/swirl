5746fc2d472f88d1795cc6c0fe9c156d7c026058
Dave Cottlehuber <dch@skunkwerks.at>
2013-07-30 02:13:31 +0200

    headers: ppspp draft-6 and rebar app imports
    - see http://tools.ietf.org/html/draft-ietf-ppsp-peer-protocol-06 for details

---
+++ include/ppspp.hrl
@@ -0,0 +1,67 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+-ifndef(PPSPP_RELEASE).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% PPSPP_RELEASE must only change wrt IETF RFC status
+-define(PPSPP_RELEASE, "d-06").
+
+%% useful bits and bytes
+-define(QWORD, 64/big).
+-define(DWORD, 32/big).
+-define(WORD,  16/big).
+-define(BYTE,   8/big).
+
+%% PPSP Protocol Options -- section 7.0
+-define(PPSPP_VERSION,	                0:?BYTE). %%% MUST, version 1:8
+-define(PPSPP_MINIMUM_VERSION,	        1:?BYTE). %%% MUST, version 1:8
+-define(PPSPP_SWARM_ID_LENGTH,          2:?BYTE). %%% MUST for initiator,
+                                                  %%% MAY for peer,
+                                                  %%% length:16, swarm_id:length
+-define(PPSPP_INTEGRITY_CHECK_METHOD,	3:?BYTE). %%% MAY, method:8
+-define(PPSPP_MERKLE_HASH_FUNCTION,	4:?BYTE). %%% MUST if Merkle tree is used
+                                                  %%% function:8
+-define(PPSPP_LIVE_SIGNATURE_ALGORITHM, 5:?BYTE). %%% MUST if CIPM is sign_all
+                                                  %%% or unified merkle_tree
+                                                  %%% algorithm:8
+-define(PPSPP_CHUNK_ADDRESSING_METHOD,	6:?BYTE). %%% MAY, chunking:8
+-define(PPSPP_LIVE_DISCARD_WINDOW,	7:?BYTE). %%% MUST for live swarms,
+                                                  %%% window: QWORD/DWORD*
+-define(PPSPP_SUPPORTED_MESSAGES,	8:?BYTE). %%% MUST unless all supported
+                                                  %%% length:8, messages:length
+-define(PPSPP_END_OPTION,             255:?BYTE). %%% 0. The key is sufficient
+-define(PPSPP_DEFAULT_CHUNK_SIZE,          1024). %%%
+
+%% PPSPP Datagram Fields -- section 8.4
+-define(PPSPP_CHANNEL_SIZE,      ?DWORD).
+-define(PPSPP_UNKNOWN_CHANNEL, 0:?DWORD).
+
+%% PPSPP Message Types -- section 8.2
+-define(PPSPP_MESSAGE_SIZE,             ?BYTE   ).
+-define(HANDSHAKE,	            << 0:?BYTE>>).
+-define(DATA,   	            << 1:?BYTE>>).
+-define(ACK,                        << 2:?BYTE>>).
+-define(HAVE,   	            << 3:?BYTE>>).
+-define(INTEGRITY,	            << 4:?BYTE>>).
+-define(PEX_RESv4,	            << 5:?BYTE>>).
+-define(PEX_REQ,	            << 6:?BYTE>>).
+-define(SIGNED_INTEGRITY,           << 7:?BYTE>>).
+-define(REQUEST,	            << 8:?BYTE>>).
+-define(CANCEL,	                    << 9:?BYTE>>).
+-define(CHOKE,	                    <<10:?BYTE>>).
+-define(UNCHOKE,	            <<11:?BYTE>>).
+-define(PEX_RESv6,	            <<12:?BYTE>>).
+-define(PEX_REScert,	            <<13:?BYTE>>).
+-define(PPSPP_MAXIMUM_MESSAGE_TYPE,   14).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-endif.
---
+++ include/swerl.hrl
@@ -0,0 +1,31 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+-include("ppspp.hrl").
+-ifndef(SWERL_PORT).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% application macros
+-define(SWERL_PORT, 7777).
+-define(SWERL_APP, swerl).
+
+-define(DEBUG_SWERL(Where, What), io:format("~s ~p~n", [Where, What])).
+
+-define(BIN2STR(Binary), helper:binary_to_string(Binary)).
+-define(CHAN2STR(Binary), helper:binary_to_string( <<Binary:?PPSPP_CHANNEL_SIZE>> )).
+
+-define(DEBUG_DGRAM_RECV(Channel), io:format("dgram: recv on channel ~s~n", [?CHAN2STR(Channel)])).
+-define(DEBUG_DGRAM_PARSE_OK(Channel), io:format("dgram: parse ok on channel ~s~n", [?CHAN2STR(Channel)])).
+
+-define(IP2STR(Host), [inet_parse:ntoa(Host)]).
+-define(ENDPOINT2STR(Host,Port), lists:flatten([?IP2STR(Peer), $:, integer_to_list(Port)])).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-endif.
