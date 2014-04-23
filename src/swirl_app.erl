0973de258be18a6f999a5879ceb2534f813e2f71
Dave Cottlehuber <dch@skunkwerks.at>
2014-04-24 01:11:50 +0200

    swirl: add types, fix bugs found by types

--- src/swirl_app.erl
+++ src/swirl_app.erl
@@ -27,6 +27,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% api
 
+-spec version() -> {string(), string()}.
 version() ->
     {ok, Version} = application:get_key(swirl, vsn),
     {?PPSPP_RELEASE, Version}.
@@ -35,16 +36,13 @@ version() ->
 %% callbacks
 
 %% used by application:start/1
+-spec start(_ ,_ ) -> {ok, pid()} | {error,_ }.
 start(_Type, _Start_Args) ->
     {Protocol, Version} = version(),
     ?INFO("swirl: protocol ~s~n", [Protocol]),
     ?INFO("swirl: version #~s~n", [Version]),
-    case swirl_sup:start_link() of
-        {ok, Pid} ->
-            {ok, Pid};
-        Other ->
-            {error, Other}
-    end.
+    swirl_sup:start_link().
 
+-spec stop(_) -> ok.
 stop(_State) ->
     ok.
