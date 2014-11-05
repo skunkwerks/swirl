f4391bb007828bc06f42c048e47b3ebfae8d19db
Dave Cottlehuber <dch@skunkwerks.at>
2014-11-05 09:39:27 +0100

    build: use custom erlang.mk with eunit support

--- erlang.mk
+++ erlang.mk
@@ -762,8 +762,7 @@ ESCRIPT_COMMENT ?= $(PROJECT)
 
 ESCRIPT_BEAMS ?= "ebin/*", \
 	"{apps,deps,lib}/*/ebin/*", \
-	"sys.config", \
-	".applist"
+	"sys.config"
 ESCRIPT_EMU_ARGS ?= -smp auto \
 	-pa . \
 	-noshell -noinput  \
@@ -889,3 +888,47 @@ build-shell-deps: $(ALL_SHELL_DEPS_DIRS)
 
 shell: build-shell-deps
 	$(gen_verbose) erl $(SHELL_PATH) $(SHELL_OPTS)
+
+# Copyright (c) 2014, Enrique Fernandez <enrique.fernandez@erlang-solutions.com>
+# This file is contributed to erlang.mk and subject to the terms of the ISC License.
+
+.PHONY: tests-eunit distclean-eunit
+
+# Configuration
+
+EUNIT_ERLC_OPTS ?= +debug_info +warn_export_vars +warn_shadow_vars +warn_obsolete_guard
+EUNIT_ERLC_OPTS += -DTEST=1 -DEXTRA=1
+
+EUNIT_OPTS ?= {dir, "ebin"}, [verbose, {report,{eunit_surefire,[{dir,"logs"}]}}]
+
+# Core targets.
+
+tests:: tests-eunit
+
+distclean:: distclean-eunit
+
+help::
+	@printf "%s\n" "" \
+		"All modules in ebin/ will be tested with eunit, results in logs/." 
+
+# Plugin-specific targets.
+
+EUNIT_RUN = erl \
+	-no_auto_compile \
+	-noshell \
+	-pa $(realpath test) $(DEPS_DIR)/*/ebin \
+	-pz $(realpath ebin) \
+	-dir test \
+	-eval 'eunit:test($(EUNIT_OPTS)).' \
+	-s init stop
+
+build-eunit:
+	$(gen_verbose) erlc -v $(EUNIT_ERLC_OPTS) -I include/ -o test/ \
+		$(wildcard src/*.erl src/*/*.erl) -pa ebin/
+
+tests-eunit: ERLC_OPTS = $(EUNIT_ERLC_OPTS)
+tests-eunit: clean deps app build-eunit
+	$(gen_verbose) $(EUNIT_RUN)
+
+distclean-eunit:
+	$(gen_verbose) rm -rf $(EUNIT_REPORT_DIR)
