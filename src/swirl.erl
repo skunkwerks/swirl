skunkwerks/swirl

commit 9f0df1b86aee48af817ca30d547900f286320b4d

swirl: add channel support
this changes everything

src/swirl.erl
@@ -13,7 +13,8 @@
 %% the License.
 
 %% @doc Library for PPSPP over UDP, aka Swift protocol
-%% <p>Wrapper to support running directly from escript.</p>
+%%
+%% Wrapper to support running directly from escript.
 %% @end
 
 -module(swirl).
@@ -21,7 +22,6 @@
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
--spec test() -> term().
 -endif.
 
 -export([main/1,
@@ -36,7 +36,10 @@
          stop_pool/2,
          start_swarm/1,
          stop_swarm/1,
+         start_channel/2,
+         stop_channel/1,
          start/0,
+         start/1,
          stop/0]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -48,6 +51,33 @@
 start() ->
     {ok, _} = application:ensure_all_started(?MODULE),
     ok.
+
+%% @doc Start the swirl application and everything ready to peer.
+%% Takes either single `swarm_id' as string or binary and assumes PPSPP
+%% defaults, or a full set of PPSPP options including the `swarm_id'.
+%% It starts a swarm and peer, and returns the URI for connecting to.
+%% @end
+-spec start(string()
+            | ppspp_options:swarm_id()
+            | ppspp_options:options()) ->
+    {ok,
+     pid(),
+     pid(),
+     inet:port_number(),
+     string()}.
+start(Swarm_id) when is_binary(Swarm_id); is_list(Swarm_id) ->
+    start(ppspp_options:use_default_options(Swarm_id));
+start(Swarm_options) ->
+    {ok, Swarm_pid} = start_swarm(Swarm_options),
+    % start a peer on a random port
+    {ok, Peer_pid}  = start_peer(0, Swarm_options),
+    %% retrieve the new port assigned to this peer
+    {ok, Peer_port} = peer_worker:pid_to_port(Peer_pid),
+    %% make a pretty URL for other systems
+    {ok, Peer_url} = peer_worker:get_url(Peer_port),
+    ?INFO("swirl: started swarm ~p and peer ~p at ~s~n",
+          [Swarm_pid, Peer_pid, Peer_url]),
+    {ok,Swarm_pid, Peer_pid, Peer_port, Peer_url}.
 
 %% @doc Stop the swirl application and all dependent swarms and peers.
 %% Allows swirl to terminate any peer connections if necessary.
@@ -126,6 +156,22 @@ stop_pool(First, Last) when is_integer(First), is_integer(Last), First < Last  -
               Ports).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% channel API
+
+%% @doc start a PPSPP channel, using the supplied peer info and swarm.
+-spec start_channel(ppspp_datagram:endpoint(), ppspp_options:options()) ->
+    {ok, pid()} | {error,_}.
+start_channel(Peer_endpoint, Swarm_options) ->
+    channel_worker:start(Peer_endpoint, Swarm_options).
+
+%% @doc stop a PPSPP channel
+%% @end
+-spec stop_channel(ppspp_channel:channel()) ->
+    ok | {error, any()}.
+stop_channel(Channel) ->
+    channel_worker:stop(Channel).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% swarm API
 
 %% @doc start a PPSPP swarm, using the supplied hash and PPSPP swarm options.
@@ -157,32 +203,28 @@ stop_swarm(Swarm) when is_binary(Swarm); is_list(Swarm) ->
 %% @end
 -spec help() -> ok.
 help() ->
-    io:format("~s: online help~n", [?SWIRL_APP]),
+    io:format("~s: online help ~p~n", [?SWIRL_APP, swirl_app:version()]),
     Help =["use any of these commands, prefixed by `swirl:` to run:",
            "",
            "help().                    these help notes",
            "start().                   starts the swirl application, but no peers or swarms",
-           "stop().                    stops the swirl application, active peers and swarms",
-           "start_swarm(Options | Id). starts a swarm using given options or swarm id",
-           "stop_swarm(Options | Id).  stops a swarm using given options or swarm id",
-           "start_peer().              starts a peer with default port & options",
-           "start_peer(Hash).          starts a peer with defaults and given hash",
-           "start_pool(First, Last).   starts peers on consecutive ports from First to Last",
-           "start_peer(Port, Options). starts a peer with given port and options",
+           "stop().                    stops the swirl application and all peers and swarms",
+           "start(Options | Id).       starts a new swarm and peer on OS selected random port",
+           "start_swarm(Opts|Id).      starts a swarm using given options or swarm id",
+           "stop_swarm(Opts|Id).       stops a swarm using given options or swarm id",
+           "start_peer(Opts|Id).       starts a peer with default port, supplied options or id",
+           "start_peer(Port, Opts|Id). starts a peer with given port",
            "stop_peer().               stops a single peer on the default port",
            "stop_peer(Port).           stops a single peer on the given port",
+           "start_pool(First, Last).   starts peers on consecutive ports from First to Last",
            "stop_pool(First, Last).    stops peers on consecutive ports from First to Last",
            "quit().                    immediately terminates the entire BEAM vm",
-           "", "",
-           "e.g. swirl:start_peer(\"c89800bfc82ed01ed6e3bfd5408c51274491f7d4\").",
            "",
+           "e.g. swirl:start(\"c89800bfc82ed01ed6e3bfd5408c51274491f7d4\").",
            "use ^c twice to exit, or type `swirl:quit().` for a graceful shutdown.",
-           ""],
+           "", ""],
 
-    lists:foreach(fun(Line) ->
-                          io:format("~s~n", [Line])
-                  end,
-                  Help),
+    lists:foreach(fun(Line) -> io:format("~s~n", [Line]) end, Help),
     ok.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -193,17 +235,3 @@ main(_) ->
     start(),
     _ = start_peer(),
     timer:sleep(infinity).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% test
-
--ifdef(TEST).
--spec peer_random_port_start_test() -> {ok, pid()}.
-peer_random_port_start_test() ->
-    start(),
-    ?assertMatch({ok, _}, start_peer(0, ppspp_options:use_default_options())).
-
--spec peer_random_port_stop_test() -> ok.
-peer_random_port_stop_test() ->
-    ?assertEqual(ok, stop_peer(0)).
--endif.

doc/overview.edoc
@@ -18,13 +18,35 @@
 
 @doc
 
+## Overview
+
+Swirl is an implementation of the IETF [PPSP] protocol, [RFC7574] in
+Erlang/OTP, and released under the [ALv2] license.
+
 ## Description
 
-Swirl is an Erlang/OTP implementation of the draft IETF [PPSP] protocol,
-under the [ALv2] license.
+> The Peer-to-Peer Streaming Peer Protocol (PPSPP) is a protocol for
+disseminating the same content to a group of interested parties in a
+streaming fashion. PPSPP supports streaming of both prerecorded (on- demand)
+and live audio/video content. It is based on the peer-to- peer paradigm,
+where clients consuming the content are put on equal footing with the
+servers initially providing the content, to create a system where everyone
+can potentially provide upload bandwidth. It has been designed to provide
+short time-till-playback for the end user and to prevent disruption of the
+streams by malicious peers. PPSPP has also been designed to be flexible and
+extensible. It can use different mechanisms to optimize peer uploading,
+prevent freeriding, and work with different peer discovery schemes
+(centralized trackers or Distributed Hash Tables). It supports multiple
+methods for content integrity protection and chunk addressing. Designed as a
+generic protocol that can run on top of various transport protocols, it
+currently runs on top of UDP using Low Extra Delay Background Transport
+(LEDBAT) for congestion control.
+
+> -- http://www.rfc-editor.org/info/rfc7574
 
 [ALv2]: http://www.apache.org/licenses/LICENSE-2.0.html
-[PPSP]: http://datatracker.ietf.org/doc/draft-ietf-ppsp-peer-protocol/
+[PPSP]: https://datatracker.ietf.org/doc/rfc7574/
+[RFC7574]: https://tools.ietf.org/html/rfc7574
 
 ## Modules ##
 

include/ppspp.hrl
@@ -15,7 +15,7 @@
 -ifndef(PPSPP_RELEASE).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% PPSPP_RELEASE must only change wrt IETF RFC status
--define(PPSPP_RELEASE, "d-10").
+-define(PPSPP_RELEASE, "rfc7574-20150710").
 
 %% useful bits and bytes
 -define(QWORD, 64/big).
@@ -44,7 +44,10 @@
 -define(PPSPP_END_OPTION,             255:?BYTE). %% 0. The key alone is sufficient
 %% values used within the option parser
 -define(PPSPP_DEFAULT_CHUNK_SIZE,          1024). %%
+-define(PPSPP_DEFAULT_MERKLE_HASH_FUN,      sha). %% agreed to change in final RFC
 -define(PPSPP_RFC_VERSION,                    1). %% match IETF protocol version
+-define(PPSPP_DEFAULT_CHUNK_METHOD, chunking_32bit_chunks). %% implied
+-define(PPSPP_DEFAULT_INTEGRITY_METHOD,  merkle_hash_tree). %% implied
 
 %% as atoms
 -define(PPSPP_ALL_PROTOCOL_OPTIONS, [supported_version,

src/channel_sup.erl
@@ -19,7 +19,7 @@
 %% functions for encoding and decoding messages.
 %% @end
 
--module(peer_sup).
+-module(channel_sup).
 -include("swirl.hrl").
 
 -ifdef(TEST).
@@ -30,7 +30,7 @@
 
 %% api
 -export([start_link/0,
-         start_child/1 ]).
+         start_child/1]).
 
 %% callbacks
 -export([init/1]).
@@ -39,20 +39,20 @@
 %% api
 
 -spec start_link() -> {ok, pid()} | ignore | {error, any()}.
-
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%%-spec start_child(port()) -> ok.
--spec start_child([inet:port_number()]) ->
+-spec start_child([ppspp_datagram:endpoint() |
+                   ppspp_options:options()]) ->
     {error,_} | {ok, pid()}.
-start_child([Port]) when is_integer(Port) ->
-    supervisor:start_child(?MODULE, [Port]).
+start_child([Peer_endpoint, Swarm_options]) ->
+    supervisor:start_child(?MODULE, [Peer_endpoint, Swarm_options]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% callbacks
 
 -spec init([]) -> {ok,{{simple_one_for_one, 10,60}, [supervisor:child_spec()] }}.
+
 init([])->
     RestartStrategy = simple_one_for_one,
     MaxRestarts = 10,
@@ -62,10 +62,10 @@ init([])->
     Shutdown = 1000,
     Type = worker,
 
-    Worker = {peer_worker, {peer_worker, start_link, []},
+    Worker = {channel_worker, {channel_worker, start_link, []},
               Restart,
               Shutdown,
               Type,
-              [peer_worker]},
+              [channel_worker]},
 
     {ok, {Options, [Worker]}}.

src/channel_worker.erl
@@ -1,0 +1,138 @@
+%% -*- tab-width: 4;erlang-indent-level: 4;indent-tabs-mode: nil -*-
+%% ex: ft=erlang ts=4 sw=4 et
+%% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+%% use this file except in compliance with the License. You may obtain a copy of
+%% the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+%% License for the specific language governing permissions and limitations under
+%% the License.
+
+%% @doc A managed PPSPP Channel that represents a remote PPSPP Peer.
+%%
+%% The state for a specific channel includes the associated remote peer,
+%% the swarm, and the channel itself. Within swirl, these three are bound
+%% together as a unique value, and the channel ID as the key. The
+%% `channel_worker' provides registration and lookups to allow a given
+%% `peer_worker' to route messages for a specific remote peer to it, given
+%% only the correct `channel_id'. This functionality is used extensively
+%% by other modules to decode, process, encode, and send PPSPP datagrams
+%% over the wire.
+%% @end
+
+-module(channel_worker).
+-include("swirl.hrl").
+-behaviour(gen_server).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+%% api
+-export([start_link/2,
+         where_is/1,
+         handle/2,
+         start/2,
+         stop/1]).
+
+%% gen_server
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+%% records and state
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% api
+
+%% @doc Post message into the mailbox.
+%% All PPSPP messages arrive through here, and then are re-dispatched back
+%% into their respective `ppspp_<message_type>.erl' modules to be processed.
+%% @end
+-spec handle(pid(), any()) -> ok.
+handle(Pid, Message) ->
+    gen_server:cast(Pid, Message).
+
+%% @doc start a PPSPP channel, using the supplied peer info.
+-spec start(ppspp_datagram:endpoint(),
+            ppspp_options:options()) -> {ok, pid()} | {error,_}.
+start(Peer_endpoint, Swarm_options) ->
+    supervisor:start_child(channel_sup, [Peer_endpoint, Swarm_options]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc Stops the server, given a channel.
+
+-spec stop(ppspp_channel:channel()) -> ok | {error, any()}.
+stop(Channel) ->
+    case where_is(Channel) of
+        {error, Reason} -> {error, Reason};
+        {ok, Pid} -> gen_server:cast(Pid, stop)
+    end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc Looks up the pid for a given channel.
+
+-spec where_is(ppspp_channel:channel()) -> {ok, pid()} | {error,_}.
+where_is(Channel)  ->
+    case Pid = gproc:lookup_local_name({?MODULE, Channel}) of
+        undefined -> {error, ppspp_channel_worker_not_found};
+        _ -> {ok, Pid}
+    end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% callbacks
+
+%% @doc Start the channel using provided peer info, typically a udp endpoint.
+%% For channel 0, i.e. the initial handshake contact from a remote peer. We
+%% effectively block any re-registration by registering this name, until the
+%% first received registration completes successfully.
+%% @end
+-spec start_link(ppspp_datagram:endpoint(), ppspp_options:swarm_id()) ->
+    ignore | {error,_} | {ok,pid()}.
+start_link(Peer, Swarm_id)  ->
+    Uri = ppspp_datagram:get_peer_uri(Peer),
+    Registration = {via, gproc, {n, l, {?MODULE, Uri}}},
+    gen_server:start_link(Registration, ?MODULE, [Peer, Swarm_id], []).
+
+%% @doc As the channel id is not known at time of process spawning, it is
+%% done during init phase, using gproc. The following values are registered:
+%% <pre lang="erlang">
+%% {{peer, Peer URI}, Peer}
+%% {{channel, Channel id}, Swarm ID}
+%% </pre>
+%% Peer is the opaque data type used in the datagram module that uniquely
+%% identifies a remote peer.
+%% TODO change registration of peers to allow multiplexed peers per remote
+%% address, and therefore multiple swarms on the same IP.
+%% TODO enable updating the registration of the URI to match the acquired
+%% channel that will accommodate all future datagrams from this peer.
+-spec init(ppspp_options:swarm_id()) -> {ok, ppspp_channel:channel()}.
+init(Swarm_id) ->
+    {ok, ppspp_channel:acquire(Swarm_id)}.
+
+%% @doc Default `gen_server' API.
+handle_call(_Request, _From, State) ->
+    {reply, ignored, State}.
+
+%% @doc Default `gen_server' API.
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%% @doc Default `gen_server' API.
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%% @doc Default `gen_server' API.
+terminate(_Reason, _State) ->
+    ok.
+
+%% @doc Default `gen_server' API.
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.

src/convert.erl
@@ -12,9 +12,10 @@
 %% License for the specific language governing permissions and limitations under
 %% the License.
 
-%% @doc conversion library
-%% <p>This module implements a set of functions to convert various
-%% erlang or binary formats to human-readable text .</p>
+%% @doc Conversion Helper Functions
+%%
+%% This module implements a set of helper functions to convert various
+%% erlang or binary formats to human-readable text.
 %% @end
 
 -module(convert).
@@ -22,7 +23,6 @@
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
--spec test() -> term().
 -endif.
 
 %% api

src/peer_worker.erl
@@ -12,17 +12,34 @@
 %% License for the specific language governing permissions and limitations under
 %% the License.
 
+%% @doc A managed PPSPP UDP-based Peer.
+%%
+%% The peer_worker provides a wrapper around the standard `gen_udp' server
+%% spawning an additional erlang process for every received datagram.
+%% At startup, the `peer_worker' registers itself in the `gproc' registry
+%% with the assigned UDP port.
+%%
+%% Note that every port has a 1-to-1 association to a specific swarm,
+%% comprising options, IP, and port. The main reason for this is
+%% to be able to pass these swarm options through to the packet decoder
+%% when checking that the requested hash is available in this swarm or not,
+%% without requiring additional lookups.
+%% @type state() = {ppspp_options:options(), inet:port_number(), port()}.
+%% @end
+
 -module(peer_worker).
 -include("swirl.hrl").
 -behaviour(gen_server).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
--spec test() -> term().
 -endif.
 
 %% api
 -export([start_link/2,
+         where_is/1,
+         pid_to_port/1,
+         get_url/1,
          stop/1]).
 
 %% callbacks
@@ -41,11 +58,10 @@
 %% api
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% @doc start the server with a port number, and ppspp options which includes
-%% the root hash and chunk addressing specification. These are required during
-%% subsequent packet parsing, even if the peer supports multiple swarms.
-%% If start_link/2 is called with a root hash, the default ppspp options are
-%% assumed.
+%% @doc Start the peer with given port number and ppspp options which
+%% includes the root hash and chunk addressing specification. These are
+%% required during subsequent packet parsing, even if the peer itself
+%% supports multiple swarms.
 -spec start_link(inet:port_number(), ppspp_options:options()) ->
     ignore | {error,_} | {ok,pid()}.
 start_link(Port, Swarm_Options) when is_integer(Port) ->
@@ -53,9 +69,9 @@ start_link(Port, Swarm_Options) when is_integer(Port) ->
     gen_server:start_link(Registration, ?MODULE,
                           [Port, Swarm_Options], []).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% @doc Stops the server.
-
+%% @doc Stops the server, given a corresponding UDP port.
+%% Returns an error tuple if the port is no longer active.
+%% @end
 -spec stop(inet:port_number()) -> ok | {error, any()}.
 stop(Port) ->
     case where_is(Port) of
@@ -63,50 +79,109 @@ stop(Port) ->
         {ok, Pid} -> gen_server:cast(Pid, stop)
     end.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% @doc Looks up the pid for a given port.
-
+%% @doc Looks up the pid for a given port
+%% Returns an error tuple if the port cannot be found in the registry.
+%% @end
 -spec where_is(inet:port_number()) -> {ok, pid()} | {error,_}.
 where_is(Port) when is_integer(Port), Port >= 0, Port =< 65535 ->
     case Pid = gproc:lookup_local_name({?MODULE, Port}) of
-        undefined -> {error, ppspp_peer_worker_not_found};
+        undefined -> {error, ppspp_peer_worker_port_not_found};
         _ -> {ok, Pid}
+    end.
+
+%% @doc Looks up the port for a given pid.
+%% Returns an error tuple if the port cannot be found in the registry.
+%% @end
+-spec pid_to_port(pid()) -> {ok, inet:port_number()} | {error,_}.
+pid_to_port(Pid) when is_pid(Pid) ->
+    % TODO change registration to make this less reliant on gproc internals
+    Gproc = proplists:get_value(gproc, gproc:info(Pid)),
+    try hd(Gproc) of
+        {{n,l,{?MODULE, Port}}, _Options } -> {ok, Port}
+    catch
+        _ -> {error, ppspp_peer_worker_pid_not_found}
+    end.
+
+%% @doc Create a URL for this endpoint that another peer could connect to.
+%% As there is no official spec for URLs yet, I made one up. See `url.md'.
+%% A simple example is ppspp://example.net:7777/c89800bf
+%% @end
+-spec get_url(inet:port_number()) -> {ok, string()}.
+get_url(Port) ->
+    {ok, Address} = get_local_ipv4_address(),
+    {ok, lists:flatten(["ppspp://", Address, ":", integer_to_list(Port), $/])}.
+
+%% @doc Retrieve the IPv4 address of this system.
+%% This is likely to be of marginal use in practice because computers are
+%% tricky and networks are downright devious. STUN or ICE or at least
+%% NAT_UPNP may prove more useful
+%% @end
+-spec get_local_ipv4_address() -> {ok, string()} | {error, any()}.
+get_local_ipv4_address() ->
+    {ok, Host} = inet:gethostname(),
+    {ok, IP} = inet:getaddr(Host, inet),
+    case inet:ntoa(IP) of
+        Resp = {error, _} -> Resp;
+        Resp -> {ok, Resp}
     end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% callbacks
 
--spec init([inet:port_number() | ppspp_options:options()]) -> {ok,[state()]}.
-init([Port, Swarm_Options]) ->
-    process_flag(trap_exit, true),
-    {ok, Socket} = gen_udp:open(Port,  [binary,
-                                        {reuseaddr, true},
-                                        {active, true} ]),
-    ?INFO("peer: ~p listening on udp:~p~n  options: ~p~n",
-          [self(), Port, Swarm_Options]),
-    {ok, [#state{port = Port, socket = Socket, options= Swarm_Options}] }.
+%% @doc Initialises the `peer_worker' and associated UDP port.
+%% Registers the worker with provided swarm options and UDP port in `gproc'.
+%% @end
 
+-spec init([inet:port_number() | ppspp_options:options()]) -> {ok,[state()]}.
+init([Requested_port, Swarm_options]) ->
+    process_flag(trap_exit, true),
+    {ok, Socket} = gen_udp:open(Requested_port,  [binary,
+                                                  {reuseaddr, true},
+                                                  {active, true} ]),
+    % If zero was passed as a port, a random one is chosen. This also
+    % must be registered in gproc, and the original port then unregistered.
+    {ok,{_IP,Assigned_port }} = inet:sockname(Socket),
+    ok = case Assigned_port =:= Requested_port of
+             true -> ok; % nothing to see, move along here
+             false -> % register a new port and drop the old one
+                 gproc:reg({n,l,{?MODULE, Assigned_port}}, Swarm_options),
+                 gproc:unregister_name({n,l,{?MODULE, 0}}),
+                 ok
+         end,
+    ?INFO("peer: ~p listening on udp:~p~n  options: ~p~n",
+          [self(), Assigned_port, Swarm_options]),
+    {ok, [#state{port = Assigned_port, socket = Socket, options= Swarm_options}] }.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% default callbacks
+
+%% @doc Default `gen_server' API.
 -spec handle_cast(stop | any(), [state()]) -> {noreply, state()}.
 handle_cast(stop, State) -> {stop, normal, State};
 handle_cast(Message, State) ->
     ?WARN("~s: unexpected cast ~p~nin state ~p~n", [?MODULE, Message, State]),
     {noreply, State}.
 
+%% @doc Default `gen_server' API.
 -spec handle_call(any(), {pid(), any()}, [state()]) -> {reply, ok, state()}.
 handle_call(Message, From, State) ->
     ?WARN("~s: unexpected call from ~p ~p~nin state ~p~n",
           [?MODULE, From, Message, State]),
     {reply, ok, State}.
 
+%% @doc Receives UDP datagrams from `gen_udp' server and spawns a subsidiary
+%% process to manage it. Returns `noreply' tuple.
 -spec handle_info(_,[state()]) -> {noreply,[state()]} | {stop,{error,{unknown_info,_}},_}.
 handle_info(Packet={udp, _Socket, _Peer, _Port, _Maybe_Datagram}, State) ->
-    proc_lib:spawn(ppspp_datagram, handle, [Packet]),
+    proc_lib:spawn(ppspp_datagram, handle_packet, [Packet]),
     {noreply, State}.
 
+%% @doc Default `gen_server' API.
 -spec code_change(_,[state()],_) -> {ok,[state()]}.
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
+%% @doc Closes down the `peer_worker'. Returns reason and brief statistics.
 -spec terminate(_,[state()]) -> ok.
 terminate(Reason, [#state{socket=Socket, port=Port}]) ->
     gproc:goodbye(),
@@ -114,22 +189,3 @@ terminate(Reason, [#state{socket=Socket, port=Port}]) ->
     {memory, Bytes} = erlang:process_info(self(), memory),
     ?INFO("peer: ~p terminating port ~p, using ~p bytes, due to reason: ~p~n",
           [self(), Port, Bytes, Reason]).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% test
-
--ifdef(TEST).
--spec start_test() -> term().
-start_test() ->
-    {ok, _} = application:ensure_all_started(swirl),
-    Swarm_Options = ppspp_options:use_default_options(),
-    {ok, Worker} = ?MODULE:start_link(0, Swarm_Options),
-    ?assertEqual(true, erlang:is_process_alive(Worker)).
-
--spec stop_test() -> term().
-stop_test() ->
-    Worker = gproc:lookup_local_name({?MODULE, 0}),
-    ?MODULE:stop(0),
-    io:format("worker is ~p~n", [Worker]),
-    ?assertEqual(false, erlang:is_process_alive(Worker)).
--endif.

commit e458fb01fb8e50e82235d3a6dde7098ae74f2a8d

swirl: fix escript support
- broken in 9f0df1b
- closes #40
- thanks @kxepal for finding this one

src/swirl.erl
@@ -227,7 +227,6 @@ help() ->
 %% for escript support
 -spec main(any()) -> no_return().
 main(_) ->
-    help(),
-    start(),
-    _ = start_peer(),
+    ok = start(),
+    {ok, _ } = start_peer(),
     timer:sleep(infinity).