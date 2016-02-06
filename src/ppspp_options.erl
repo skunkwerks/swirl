dac9c231f90f489b69ffd586986d295bd24184ec
Dave Cottlehuber <dch@skunkwerks.at>
2016-02-06 01:14:06 +0100

    options: maps

--- src/ppspp_options.erl
+++ src/ppspp_options.erl
@@ -40,20 +40,19 @@
          use_default_options/0,
          use_default_options/1]).
 
+%% types largely as defined in PPSPP RFC
+
 -export_type([swarm_id/0,
               merkle_tree_hash_function/0,
               content_integrity_protection_method/0,
               options/0,
               option/0]).
 
-%% @type swarm_id() = binary(). Note that could be restricted further.
+%% @type swarm_id() = binary(). TODO could be restricted further.
 -opaque swarm_id() :: binary().
 
-%% @type options() = {options, options_dict()}.
--opaque options() :: {options, options_dict()}.
-
-%% @type options_dict() = list({ option(), any()}).
--type options_dict() :: list({ option(), any()}).
+%% @type options() = #{ option() => any()}. TODO could be restricted further.
+-opaque options() :: #{ option() => any()}.
 
 %% @type option() = supported_version
 %% | minimum_version
@@ -65,18 +64,19 @@
 %% | supported_messages
 %% | end_option
 %% | default_chunk_size
-%% | ppspp_chunk:addressing_method().
+%% | chunk_addressing_method.
 -opaque option() :: supported_version
 | minimum_version
 | swarm_id
 | content_integrity_check_method
 | merkle_hash_tree_function
 | live_signature_algorithm
-| ppspp_chunk:addressing_method()
+| chunk_addressing_method
 | live_discard_window
 | supported_messages
 | end_option
-| default_chunk_size.
+| default_chunk_size
+| chunk_addressing_method.
 
 %% Matches a subset of types from http://erlang.org/doc/man/crypto.html
 %%  digest_type() =  md5 | sha | sha224 | sha256 | sha384 | sha512, and
@@ -95,41 +95,37 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc Unpack PPPSPP options as encoded in wire format into erlang terms.
--spec unpack(binary()) -> {binary(), options()} | {error, atom()}.
+-spec unpack(binary()) -> {binary(), options()} | {error, any()}.
 unpack(Maybe_Options) ->
-    {Maybe_Messages, Options} = unpack(Maybe_Options, orddict:new()),
-    {Maybe_Messages, {options, Options}}.
--spec unpack(binary(), options_dict()) -> { binary(), options_dict()}.
+    {Maybe_Messages, Options} = unpack(Maybe_Options, #{}),
+    {Maybe_Messages, Options}.
+-spec unpack(binary(), options()) -> { binary(), options()}.
 unpack( <<?PPSPP_SUPPORTED_VERSION,
           Version:?BYTE,
-          Maybe_Options/binary >>, Options0) ->
-    Options = orddict:store(supported_version, Version, Options0),
-    unpack(Maybe_Options, Options);
+          Maybe_Options/binary >>, Options) ->
+    unpack(Maybe_Options, Options#{supported_version => Version});
 unpack( <<?PPSPP_MINIMUM_VERSION,
           Version:?BYTE,
-          Maybe_Options/binary >>, Options0) ->
-    Options = orddict:store(minimum_version, Version, Options0),
-    unpack(Maybe_Options, Options);
+          Maybe_Options/binary >>, Options) ->
+    unpack(Maybe_Options, Options#{minimum_version => Version});
 unpack( <<?PPSPP_SWARM_ID_LENGTH,
           Length:?WORD,
           Swarm_ID:Length/binary,
-          Maybe_Options/binary >>, Options0) ->
-    Options = orddict:store(swarm_id, Swarm_ID, Options0),
-    unpack(Maybe_Options, Options);
+          Maybe_Options/binary >>, Options) ->
+    unpack(Maybe_Options, Options#{swarm_id => Swarm_ID});
 unpack( <<?PPSPP_INTEGRITY_CHECK_METHOD,
           Maybe_Method:?BYTE,
-          Maybe_Options/binary >>, Options0) ->
+          Maybe_Options/binary >>, Options) ->
     Method = case Maybe_Method of
                  0 -> no_integrity_protection;
                  1 -> merkle_hash_tree;
                  2 -> sign_all;
                  3 -> unified_merkle_hash_tree
              end,
-    Options = orddict:store(content_integrity_check_method, Method, Options0),
-    unpack(Maybe_Options, Options);
+    unpack(Maybe_Options, Options#{content_integrity_check_method => Method});
 unpack( <<?PPSPP_MERKLE_HASH_FUNCTION,
           Maybe_Hash_Function:?BYTE,
-          Maybe_Options/binary >>, Options0) ->
+          Maybe_Options/binary >>, Options) ->
     Hash_Function = case Maybe_Hash_Function of
                         0 -> sha;
                         1 -> sha224;
@@ -137,21 +133,19 @@ unpack( <<?PPSPP_MERKLE_HASH_FUNCTION,
                         3 -> sha384;
                         4 -> sha512
                     end,
-    Options = orddict:store(merkle_hash_function, Hash_Function, Options0),
-    unpack(Maybe_Options, Options);
+    unpack(Maybe_Options, Options#{merkle_hash_function => Hash_Function});
 unpack( <<?PPSPP_LIVE_SIGNATURE_ALGORITHM,
           Maybe_Algorithm:?BYTE,
-          Maybe_Options/binary >>, Options0) ->
+          Maybe_Options/binary >>, Options) ->
     Algorithm = case Maybe_Algorithm of
                     %% TODO we can parse this yet but need normative algorithm names
                     0 -> live_signature_algorithm_not_yet_implemented
                 end,
-    Options = orddict:store(merkle_hash_tree_algorithm, Algorithm, Options0),
-    unpack(Maybe_Options, Options);
+    unpack(Maybe_Options, Options#{merkle_hash_tree_algorithm => Algorithm});
 %% TODO this should probably move to ppspp_chunk.erl eventually
 unpack( <<?PPSPP_CHUNK_ADDRESSING_METHOD,
           Maybe_Method:?BYTE,
-          Maybe_Options/binary >>, Options0) ->
+          Maybe_Options/binary >>, Options) ->
     Method = case Maybe_Method of
                  0 -> chunk_32bit_bins;
                  1 -> chunk_64bit_bytes;
@@ -159,16 +153,15 @@ unpack( <<?PPSPP_CHUNK_ADDRESSING_METHOD,
                  3 -> chunk_64bit_bins;
                  4 -> chunk_64bit_chunks
              end,
-    Options = orddict:store(chunk_addressing_method, Method, Options0),
-    unpack(Maybe_Options, Options);
+    unpack(Maybe_Options, Options#{chunk_addressing_method => Method});
 %% discard window is nasty as it has variable size based on pre-conditions:
 %% - the chunk addressing method must have been specified already
 %% - the window size depends on the addressing method already used
 %% so the requirement here is that these are specified in the already-
 %% parsed options. Serendipitously we have those in the accumulator.
 unpack( <<?PPSPP_LIVE_DISCARD_WINDOW, Maybe_Discard_Window/binary>>,
-        Options0) ->
-    Length = case orddict:fetch(chunk_addressing_method, Options0) of
+        #{chunk_addressing_method := Chunking_Method} = Options) ->
+    Length = case Chunking_Method of
                  chunk_32bit_bins   -> 32;
                  chunk_64bit_bytes  -> 64;
                  chunk_32bit_chunks -> 32;
@@ -181,79 +174,71 @@ unpack( <<?PPSPP_LIVE_DISCARD_WINDOW, Maybe_Discard_Window/binary>>,
                  16#ffffffffffffffff -> ppspp_live_discard_window_keep_all_chunks;
                  _ -> Window0
              end,
-    Options = orddict:store(ppspp_live_discard_window, Window, Options0),
-    unpack(Maybe_Options, Options);
+    unpack(Maybe_Options, Options#{ppspp_live_discard_window => Window});
 unpack( <<?PPSPP_SUPPORTED_MESSAGES,
           Length:?BYTE,
           Maybe_Supported_Message_Types:Length/big,
-          Maybe_Options/binary >>, Options0) ->
+          Maybe_Options/binary >>, Options) ->
     Supported_Message_Types = case Maybe_Supported_Message_Types of
                                   %% TODO probably needs a custom bitmatching parser
                                   _ -> ppspp_supported_messages_not_yet_implemented
                               end,
-    Options = orddict:store(ppspp_supported_message_types,
-                            Supported_Message_Types, Options0),
-    unpack(Maybe_Options, Options);
+    unpack(Maybe_Options,
+           Options#{ppspp_supported_message_types => Supported_Message_Types});
 unpack( <<?PPSPP_END_OPTION, Maybe_Messages/binary>>, Options) ->
     {Maybe_Messages, Options};
-%% if PPPSPP_END_OPTION is not present in the binary, bad things** happen
+%% if PPPSPP_END_OPTION is not present in the binary, bad things** happen as
+%% we run over the end of the buffer consuming whatever else is after options.
 unpack( <<>>, _Options) -> {error, ppspp_invalid_options}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% @doc Provides a clean interfaces to retrieve a given option from the
-%% opaque store.
-%% @end
--spec get(option(), options()) -> any().
-get(Option, {options, Options_Dict}) ->
-    orddict:fetch(Option, Options_Dict).
-
 %% @doc Returns chunking method for the swarm
+-spec get_chunk_addressing_method(options()) -> any().
 get_chunk_addressing_method(Options) ->
-    get(chunk_addressing_method, Options).
+    maps:get(chunk_addressing_method, Options).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc Returns CIPM for the provided swarm
 -spec get_content_integrity_check_method(options()) -> any().
-
 get_content_integrity_check_method(Options) ->
-    get(content_integrity_check_method, Options).
+    maps:get(content_integrity_check_method, Options).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc Returns the hash function for the provided swarm
 -spec get_merkle_hash_tree_function(options()) -> any().
-
 get_merkle_hash_tree_function(Options) ->
-    get(merkle_hash_tree_function, Options).
+    maps:get(merkle_hash_tree_function, Options).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc Returns the lowest accepted PPSP version for the swarm.
 -spec get_minimum_version(options()) -> any().
-
 get_minimum_version(Options) ->
-    get(minimum_version, Options).
+    maps:get(minimum_version, Options).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc Returns the embedded swarm_id.
 -spec get_swarm_id(options()) -> swarm_id().
-
 get_swarm_id(Options) ->
-    get(swarm_id, Options).
+    maps:get(swarm_id, Options).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc Returns the swarm chunk size
 -spec get_chunk_size(options()) -> pos_integer().
 get_chunk_size(Options) ->
-    get(chunk_size, Options).
+    maps:get(chunk_size, Options).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc Returns the highest accepted PPSP version for the swarm.
 -spec get_maximum_supported_version(options()) -> any().
-
 get_maximum_supported_version(Options) ->
-    get(supported_version, Options).
+    maps:get(supported_version, Options).
 
-%% @doc helper unwrapper to pull out components from a datagram orddict
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc helper unwrapper to pull out components from a datagram map
 %% @end
--spec get_options(orddict:orddict()) -> options().
-get_options(Dict) -> {options, orddict:fetch(options, Dict)}.
+%% FIXME this can probably be deleted in its entirety
+-spec get_options(#{}) -> options().
+get_options(#{options := Options}) -> Options.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc Provides standard options from the PPSPP RFC as erlang terms.
@@ -263,13 +248,14 @@ get_options(Dict) -> {options, orddict:fetch(options, Dict)}.
 %% in hex. Blank swarm options are used for starting peer_workers that may
 %% handle many peer swarms concurrently on different channels. It returns:
 %% <pre lang="erlang">
-%% {options,[{chunk_addressing_method,chunking_32bit_chunks},
-%%           {chunk_size,1024},
-%%           {content_integrity_check_method,merkle_hash_tree},
-%%           {merkle_hash_tree_function,sha},
-%%           {minimum_version,1},
-%%           {swarm_id, your_swarm_id_here },
-%%           {supported_version,1}]}.
+%% options => #{
+%%        chunk_addressing_method => chunking_32bit_chunks,
+%%        chunk_size => 1024,
+%%        content_integrity_check_method => merkle_hash_tree,
+%%        merkle_hash_tree_function => sha256,
+%%        minimum_version => 1,
+%%        supported_version => 1,
+%%        swarm_id => << ... >>}.
 %% </pre>
 -spec use_default_options() -> options().
 use_default_options() ->
@@ -280,14 +266,13 @@ use_default_options() ->
 use_default_options(Hex_String) when is_list(Hex_String) ->
     use_default_options(convert:hex_string_to_padded_binary(Hex_String));
 use_default_options(Swarm_id) when is_binary(Swarm_id) ->
-    {options,
-     orddict:from_list( [{swarm_id, Swarm_id},
-                         {chunk_addressing_method, ?PPSPP_DEFAULT_CHUNK_METHOD},
-                         {chunk_size, ?PPSPP_DEFAULT_CHUNK_SIZE},
-                         {content_integrity_check_method, ?PPSPP_DEFAULT_INTEGRITY_METHOD},
-                         {merkle_hash_tree_function, ?PPSPP_DEFAULT_MERKLE_HASH_FUN},
-                         {minimum_version, ?PPSPP_RFC_VERSION},
-                         {supported_version, ?PPSPP_RFC_VERSION}])}.
+    #{swarm_id                       => Swarm_id,
+      chunk_addressing_method        => ?PPSPP_DEFAULT_CHUNK_METHOD,
+      chunk_size                     => ?PPSPP_DEFAULT_CHUNK_SIZE,
+      content_integrity_check_method => ?PPSPP_DEFAULT_INTEGRITY_METHOD,
+      merkle_hash_tree_function      => ?PPSPP_DEFAULT_MERKLE_HASH_FUN,
+      minimum_version                => ?PPSPP_RFC_VERSION,
+      supported_version              => ?PPSPP_RFC_VERSION}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc Provides standard options from the PPSPP RFC as erlang terms.
@@ -297,10 +282,11 @@ use_default_options(Swarm_id) when is_binary(Swarm_id) ->
 %% agreed swarm options that are assigned to this particular swarm / channel.
 -spec use_minimum_options() -> options().
 use_minimum_options() ->
-    {options, orddict:from_list([{minimum_version, ?PPSPP_RFC_VERSION}])}.
+    #{minimum_version => ?PPSPP_RFC_VERSION}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% @doc Pack an orddict of erlang terms into a binary PPSPP message segment
+%% <ul>
+%% @doc Pack a map of erlang terms into a binary PPSPP message segment
 -spec pack(options()) -> binary().
 
 pack(_) -> <<>>.
